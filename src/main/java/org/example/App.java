c31ee618231489d86b1b3bcf602f5fdac6790a69
saranya <saranyasanjana09@gmail.com>
2026-08-02 23:21:35 +0530

    first-student-created- successfully

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main( String[] args ) {
         ApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");
-        System.out.println( "Hello World!" );
-        Student student = context.getBean("st1", Student.class);
-        student.show();
+//        System.out.println( "Hello World!" );
+//        Student student = context.getBean("st1", Student.class);
+//        student.show();
     }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Student {
+    public Student(){
+       System.out.println("student is created..");
+    }
     public void show(){
         System.out.println("In show method");
     }
