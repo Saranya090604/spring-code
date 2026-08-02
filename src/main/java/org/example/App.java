39ea18078858438beb69e1904032647fddd3dfdd
saranya <saranyasanjana09@gmail.com>
2026-08-02 23:08:42 +0530

    First application context program

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,9 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main( String[] args ) {
-//        ApplicationContext context = new ClassPathXmlApplicationContext();
+        ApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");
         System.out.println( "Hello World!" );
-        Student student = new Student();
+        Student student = context.getBean("st1", Student.class);
         student.show();
     }
 }
