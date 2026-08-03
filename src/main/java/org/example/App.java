941fe64b4283595ebdd2810ec4240887774462a6
saranya <saranyasanjana09@gmail.com>
2026-08-03 10:23:28 +0530

    setter-injection-method

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main( String[] args ) {
         ApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");
-//        System.out.println( "Hello World!" );
-//        Student student = context.getBean("st1", Student.class);
-//        student.show();
+        Student student = context.getBean("st1", Student.class);
+        student.age = 25;
+        System.out.println(student.age);
+        student.show();
     }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -4,6 +4,7 @@ public class Student {
     public Student(){
        System.out.println("student is created..");
     }
+    int age;
     public void show(){
         System.out.println("In show method");
     }
