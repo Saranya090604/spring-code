4d03b72fd7a646bcc8e3d587b8fb1dee66639355
saranya <saranyasanjana09@gmail.com>
2026-08-18 23:56:18 +0530

    setter-injection-pen

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,8 +7,6 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");
         Student student = context.getBean("st1", Student.class);
-//        student.setAge(23);
-//        System.out.println(student.getAge());
-        student.writeExam();
+        student.pen();   // calls the pen() method we’ll add in Student.java
     }
 }
