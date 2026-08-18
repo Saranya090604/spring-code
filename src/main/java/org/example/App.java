ca2f0d22ed4be80086fac0f6314ae9aacd564666
saranya <saranyasanjana09@gmail.com>
2026-08-18 22:19:21 +0530

    setter-injection

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");
         Student student = context.getBean("st1", Student.class);
         student.setAge(23);
