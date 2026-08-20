925f8c09b6c5b223adc39e99a59c7e9bc9d2bc56
saranya <saranyasanjana09@gmail.com>
2026-08-20 21:31:14 +0530

    constructor-injection-pencil-sketch

---
+++ src/main/java/org/example/Ink.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Ink {
+        private String color;
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public Ink() {
+            System.out.println("Ink is ready..");
+        }
+}
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -3,7 +3,15 @@ package org.example;
 public class Student {
 //    private int age;
 //    private int rno;
-    private Writer writer = new Pencil();
+    private Writer writer;
+
+    public Student(Writer writer) {
+        this.writer = writer;
+    }
+    public Student(){
+
+    }
+
     public void writeExam(){
         writer.write();
     }
