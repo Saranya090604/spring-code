34cf8c493b499d282e45219e031d346a72c7fc50
saranya <saranyasanjana09@gmail.com>
2026-08-20 20:47:21 +0530

    constructor-injection-pencil-sketch

---
+++ src/main/java/org/example/Pencil.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Pencil extends Sketch{
+    public void write(){
+        System.out.println("Writing using pencil");
+    }
+
+}
---
+++ src/main/java/org/example/Sketch.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Sketch implements Writer {
+    @Override
+    public void write() {
+        System.out.println("Writing using sketch");
+    }
+}
